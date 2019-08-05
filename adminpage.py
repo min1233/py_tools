405e5f047eb245e26549e12512fd3c7680dd5758
min1233 <minsug937@naver.com>
2019-08-05 03:25:47 -0400

    color add

--- adminpage.py
+++ adminpage.py
@@ -1,22 +1,35 @@
 import requests
 import time
 
-url=''
+end = "\033[0m"
+bold = "\033[1m"
+red = "\033[31m"
+green = "\033[32m"
+
+url='http://url'
 
 f = open('admin_page_list', mode='r')
 page = f.read().split()
 port = "80"
-result = ''
+result = '\n\n\n----------------result----------------\n\n\n'
 
 for i in range(len(page)):
-    temp = url+":"+port+"/"+page[i]
-    result += temp+"\n"
-    response = requests.get(temp)
-    text = response.text
-    if(text.find("404")!=-1):
-        result += "Not Found\n"
-    else:
-        result += text
-        break
+    try:
+        temp = url+":"+port+"/"+page[i]
+        response = requests.get(temp)
+        text = response.text
+        if(text.find("404")!=-1):
+            print("\n"+bold+red+"[-]"+end+" "+temp)
+            print("Not Found\n")
+        else:
+            result += bold+green+"[+]"+end+" "+temp
+            result += text
+            print("\n"+bold+green+"[+]"+end+" "+temp)
+            print(text)
+            break
+    except:
+        print("\n"+bold+red+"[-]"+end+" "+temp)
+        print("Error\n")
     #time.sleep(5)
 print(result)
+
