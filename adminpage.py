0d332254cbc761fa78f6f0f14cc7bd3a21a9f251
min1233 <minsug937@naver.com>
2019-08-09 00:37:42 -0400

    Version 1.1 -> add robots.txt find function

--- adminpage.py
+++ adminpage.py
@@ -2,6 +2,13 @@ import requests
 import time
 import sys
 
+def menu():
+    print("-"*30)
+    print("1. admin page find tools")
+    print("2. robots find tools")
+    print("-"*30)
+    return input(">> ")
+
 def argu_check():
     argu_size = len(sys.argv)
     if(argu_size==1):
@@ -21,7 +28,8 @@ def file_type_check(): # File type check
     f = open('page_list', mode='r')
     tmp = f.read().split()
     argu_size = len(sys.argv) # include allow file type in arguments
-    file_type = ["htm","cfm","brf","cgi","js"]
+    #file_type = ["htm","cfm","brf","cgi","js"]
+    file_type = ["htm","js"]
     page = []
     if argu_size!=1:
         for i in range(0,len(tmp)):
@@ -51,7 +59,6 @@ def admin_check(url,port,page,path,error_text): # check admin page (url:port/pag
     true_false = error_text!=""
 
     result = "----------------result----------------\n"
-
     for i in range(len(page)):
         try:
             temp = url+":"+port+"/"+path+page[i]
@@ -69,24 +76,50 @@ def admin_check(url,port,page,path,error_text): # check admin page (url:port/pag
                 result += text+"\n"
                 print("\n"+bold+green+"[+]"+end+" "+temp)
                 print(text+"\n")
-        except:
+        except Exception as ex:
             print("\n"+bold+red+"[-]"+end+" "+temp)
             print("Error\n")
+            print(ex)
 
     result += "--------------------------------------\n"
     print(result)
     return result
 
-def save(url,result):
-    s_index = url.find("//")+2
-    print("Save result, File Path : "+url[s_index:])
-    f = open(url[s_index:],"w")
+def robots(port):
+    f = open("url_list","r")
+    url = f.read().split()
+    result = "----------------result----------------\n\n"
+    for i in range(len(url)):
+        temp = url[i]+":"+port+"/robots.txt"
+        result += temp+"\n"
+        try:
+            response = requests.get(temp)
+            text = response.text
+            result += text+"\n\n"
+        except:
+            result += "Not Found"+"\n"
+    result += "--------------------------------------\n"
+    print("\nrobots.txt Result")
+    print(result)
+    return result
+
+def save(file_name,result):
+    file_name = file_name.replace(":","")
+    file_name = file_name.replace("/","_")
+    print("Save result, File Path : "+file_name)
+    f = open(file_name,"w")
     f.write(result.encode('utf-8')) # fix up breaking hangul
     f.close()
 
-path= raw_input("Please enter the path that append when the program find admin page.( Ex : homepage/ )\nIf you don`t want to use this function, Press the Enter : ")
-error_text= raw_input("\nPleas enter the text that appeares when the program can`t find admin page.\nIf you want to use the default value, Press the Enter (default=404) : ")
-url,port = argu_check()
-page = file_type_check()
-result = admin_check(url,port,page,path,error_text)
-save(url,result)
+num = menu()
+if num==1:
+    path= raw_input("Please enter the path that append when the program find admin page.( Ex : homepage/ )\nIf you don`t want to use this function, Press the Enter : ")
+    error_text= raw_input("\nPleas enter the text that appeares when the program can`t find admin page.\nIf you want to use the default value, Press the Enter (default=404) : ")
+    url,port = argu_check()    
+    page = file_type_check()
+    result = admin_check(url,port,page,path,error_text)
+    save(url[s_index:]+path,result)
+elif num==2:
+    port = raw_input("Please enter the scan port : ")
+    result = robots(port)
+    save("robots.txt",result)
