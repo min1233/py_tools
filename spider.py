d5a2ee014a37eaa634b1a0fd4b9eb3884be4eece
min1233 <minsug937@naver.com>
2019-09-19 11:41:33 +0900

    Version 2.0.1 -> change Regular

--- spider.py
+++ spider.py
@@ -2,7 +2,11 @@ import urllib2
 import re
 import threading
 import time
+import sys
 from collections import OrderedDict # remove duplication in Order
+
+reload(sys) # fix bug - ascii range(128)
+sys.setdefaultencoding('utf-8') # fix bug - ascii range(128)
 
 lock = threading.Lock()
 t_max = 20
@@ -14,6 +18,7 @@ url_list = [url]
 
 file_name = url.replace(":","")
 file_name = file_name.replace("/","_")
+file_name = file_name.replace(".","_")
 f = open("./spider/"+file_name,"w")
 
 def debug(url_list):
@@ -22,7 +27,6 @@ def debug(url_list):
 		print(c)
 	print("-"*50)
 
-
 def debug2(url,url_path1,url_path2):
 	print(url)
 	print(url_path1.encode("utf-8"))
@@ -30,7 +34,7 @@ def debug2(url,url_path1,url_path2):
 
 def file_type_check(url,check_list):
         for file_type in check_list:
-                if(url.find(file_type)!=-1):
+                if(url.lower().find(file_type)!=-1):
                         return 1
         return 0
 
@@ -69,8 +73,6 @@ def slush_remove(path):
 	if(path[0]=="/"): path = path[1:]
 	return path
 
-
-
 def url_request(url):
 	try:
 		rep = urllib2.urlopen(url,timeout=5)
@@ -84,13 +86,14 @@ def reg(data,tmp):
 	tmp1 = [] # url_list
 	index1,url_path1,url_path2 = url_process1(tmp) # http:// start index, http://example.com/, http://example.com/path/
 
-	pattern = re.compile("http[^\"\'\n?<>?,`]*")
-	pattern2 = re.compile("[\"\']/+[^\"\'\n<>?,`]*")
-	pattern3 = re.compile("[\"\'][\.]+/[^\"\'\n<>?,`]*")
+	pattern = re.compile("http[^\"\'\n<>,`;|]*") # chagne Regular
+	pattern2 = re.compile("[\"\']/+[\w\#\?\.\-&=_%/()]*") # change Regular
+	pattern3 = re.compile("[\"\'][\.]+/[\w\#\?\.\-&=_%/()]*") # chagne Regular
 
 	tmp2 = list(OrderedDict.fromkeys(pattern.findall(data))) # http://example.com
 	tmp3 = list(OrderedDict.fromkeys(pattern2.findall(data))) # http://example.com
 	tmp4 = list(OrderedDict.fromkeys(pattern3.findall(data))) # http://example.com
+
 
 	for c in tmp2:
 		url = c.replace("\\","")
@@ -110,7 +113,7 @@ def reg(data,tmp):
 def check(url_list,find_list):
 	lock.acquire()
 	return_value = []
-	check_list = [".jpg",".png",".gif",".pdf",".mp4",".exe",".tgz",".ids",".xls"]
+	check_list = [".jpg",".png",".gif",".pdf",".mp4",".exe",".tgz",".ids",".xls",".eot",".woff",".otf",".ttf",".svg"]
 	for url in find_list:
 		path1 = re.compile(":[0-9]+") # find port regex
 		port1 = path1.findall(url)
@@ -169,7 +172,7 @@ for i,tmp in enumerate(url_list):
 		th.start()
 		threads.append(th)
 	else:
-		print("\nCount : "+str(i)+" / len(url_list) : "+str(len(url_list)-1))
+		print("\nCount : "+str(i+1)+" / len(url_list) : "+str(len(url_list)))
 		th_main(tmp)
 
 	if(count==t_max or not(true_false2)):
@@ -184,6 +187,6 @@ for th in threads:
 f.close()
 debug(url_list)
 
-print("Search Count : "+str(i))
+print("Search Count : "+str(i+1))
 print("end")
 print(time.time()-start)	
